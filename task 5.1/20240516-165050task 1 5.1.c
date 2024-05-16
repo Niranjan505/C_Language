5a62b20e397dedcf8b62939403f31400d7afacbd
Niranjan_Goswami <goswaminiranjan505@gmail.com>
2024-05-16 17:37:37 +0530

    Add files via upload

---
+++ task 5.1/20240516-165050task 1 5.1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+
+main()
+{  int num1,num2;
+  clrscr();
+  
+  printf("Enter a value of the first number: ");
+  scanf("%d",&num1);
+  
+  printf("Enter a value of the second number: ");
+  scanf("%d",&num2);
+   if(num1<num2)
+   {
+      printf("The minimum value is: %d",num1);
+   }
+   else
+   { 
+      printf("The minimum value is: %d",num2);
+   } 
+
+ getch();
+}
---
+++ task 5.1/TASK51.BAK
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+main()
+{
+  int a;
+   clrscr();
+   printf("Eentar any numbar:");
+   scanf("%d",&a);
+   if(a>0)
+   {
+    printf("This numbar is positive");
+   }
+   else if(a=0)
+   {
+    printf("This numbar is nutaral");
+   }
+   else
+   {
+    printf("This numbar is negative");
+   }
+   getch();
+}
