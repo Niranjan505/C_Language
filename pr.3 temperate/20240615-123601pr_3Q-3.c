1d8d58dbd02f0c88ce0556038e70be2a4c046ddd
Niranjan_Goswami <goswaminiranjan505@gmail.com>
2024-06-15 12:43:04 +0530

    Add files via upload

---
+++ pr.3 temperate/20240615-123601pr_3Q-3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <conio.h>
+
+main() 
+{
+    int n, firstdigit, lastdigit, sum;
+    clrscr();
+    
+    printf("Enter any number: ");
+    scanf("%d", &n);
+
+    firstdigit = n;
+    while (firstdigit >= 10)
+    {
+        firstdigit /= 10;
+    }
+
+    lastdigit = n % 10;
+
+    sum = firstdigit + lastdigit;
+
+    printf("The sum of the first and the last digit: %d\n", sum);
+
+}
